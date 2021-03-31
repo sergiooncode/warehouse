720376608281ad9f0594941384fadb11014b0bff
sperez <sperez@savanamed.com>
2021-03-31 21:04:54 +0200

    Fix field type

--- backend/src/infrastructure/persistence/mongoengine/model/canonical_article.py
+++ backend/src/infrastructure/persistence/mongoengine/model/canonical_article.py
@@ -1,7 +1,8 @@
 from mongoengine import EmbeddedDocument
 from mongoengine import IntField
+from mongoengine import StringField
 
 
 class MongoCanonicalArticle(EmbeddedDocument):
-    article_id = IntField()
+    article_id = StringField(max_length=20)
     needed_amount = IntField()
