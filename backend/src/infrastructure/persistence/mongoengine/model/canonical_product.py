747296ad7eca403a0d3666b74089b194d0569b66
sperez <sperez@savanamed.com>
2021-03-31 21:06:47 +0200

    Add unnecessary import so MongoCanonicalArticle is registered

--- backend/src/infrastructure/persistence/mongoengine/model/canonical_product.py
+++ backend/src/infrastructure/persistence/mongoengine/model/canonical_product.py
@@ -3,6 +3,10 @@ from mongoengine import EmbeddedDocumentListField
 from mongoengine import FloatField
 from mongoengine import StringField
 
+from src.infrastructure.persistence.mongoengine.model.canonical_article import (
+    MongoCanonicalArticle,
+)  # noqa
+
 
 class MongoCanonicalProduct(Document):
     name = StringField(max_length=100)
